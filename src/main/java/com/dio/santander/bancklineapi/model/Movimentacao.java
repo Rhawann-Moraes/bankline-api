a8d884888976b77c0e2d8b2a0db823e93c10a39e
Rhawann Moraes <93962806+Rhawann-Moraes@users.noreply.github.com>
2022-05-05 17:42:53 -0300

    Mapeamento de Classe
    Criei as classes relacionadas ao contexto de modelo da aplicação de Bankline

---
+++ src/main/java/com/dio/santander/bancklineapi/model/Movimentacao.java
@@ -0,0 +1,41 @@
+package com.dio.santander.bancklineapi.model;
+
+public class Movimentacao {
+	private Integer id;
+	private LocalDatetime dataHora;
+	private String descricao;
+	private Double valor;
+	private MovimentacaoTipo tipo;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public LocalDatetime getDataHora() {
+		return dataHora;
+	}
+	public void setDataHora(LocalDatetime dataHora) {
+		this.dataHora = dataHora;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	public MovimentacaoTipo getTipo() {
+		return tipo;
+	}
+	public void setTipo(MovimentacaoTipo tipo) {
+		this.tipo = tipo;
+	}
+	
+}
