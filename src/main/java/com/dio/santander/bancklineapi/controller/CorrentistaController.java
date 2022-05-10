5ae9b0cea1fb6a70a2f3e6c53f1970312b5b0f29
Rhawann Moraes <93962806+Rhawann-Moraes@users.noreply.github.com>
2022-05-09 22:46:03 -0300

    conectando com o postgresql

---
+++ src/main/java/com/dio/santander/bancklineapi/controller/CorrentistaController.java
@@ -0,0 +1,23 @@
+package com.dio.santander.bancklineapi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.dio.santander.bancklineapi.model.Correntista;
+import com.dio.santander.bancklineapi.repository.CorrentistaRepository;
+
+@RestController
+@RequestMapping(" /correntistas")
+public class CorrentistaController {
+		@Autowired
+		private CorrentistaRepository repository;
+		
+		@GetMapping
+		public List<Correntista> findAll(){
+			return repository.findAll(); 
+		}
+		
+}
